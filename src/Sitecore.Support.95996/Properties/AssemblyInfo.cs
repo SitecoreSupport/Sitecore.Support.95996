b3580afdd45f3b3203eb772b3624bb1236e58dd5
DK\AMCH <amch@sitecore.net>
2017-11-01 13:09:11 +0200

    initial commit

---
+++ src/Sitecore.Support.95996/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95996")]
+[assembly: AssemblyProduct("Sitecore.Support.95996")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.95996/Shell/Applications/ContentManager/Dialogs/ResetFields/ResetFieldsForm.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Shell;
+using System;
+
+namespace Sitecore.Support.Shell.Applications.ContentManager.Dialogs.ResetFields
+{
+    public class ResetFieldsForm : Sitecore.Shell.Applications.ContentManager.Dialogs.ResetFields.ResetFieldsForm
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            Assert.ArgumentNotNull(e, "e");
+            base.OnLoad(e);
+            if (!Context.ClientPage.IsEvent)
+            {
+                base.OK.ToolTip = Translate.Text("Reset the field values on this page.");
+                Item itemFromQueryString = UIUtil.GetItemFromQueryString(Context.ContentDatabase);
+                RenderEditor(itemFromQueryString, itemFromQueryString, this.Fields);
+            }
+        }
+    }
+}
